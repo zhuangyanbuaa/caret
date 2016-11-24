a4f50673ed55f1070d02bb85f070a28e80516bb3
zhuangyanbuaa <zhuangyanbuaa@gmail.com>
2016-11-24 11:14:59 +0900

    Update caret_VariableImportance.R

--- caret_VariableImportance.R
+++ caret_VariableImportance.R
@@ -1,1 +1,18 @@
+### Variable Importance
+
+# 分为用模型和不用模型
+# 如果不用模型或模型没有评价importance的指标，则用filter的方法
+
+# varImp利用模型指标来测定变量重要度
+gbmImp <- varImp(gbmFit3, scale = FALSE)
+gbmImp
+
+# 得到ROC曲线
+roc_imp <- filterVarImp(x = training[, -ncol(training)], y = training$Class)
+head(roc_imp)
+
+# 画出重要度的图
+plot(gbmImp, top = 20)
+
+
 
